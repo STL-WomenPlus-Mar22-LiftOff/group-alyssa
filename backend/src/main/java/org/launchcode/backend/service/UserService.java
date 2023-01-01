f920203faf4dabbb55896cf9a5f657015f52c3fe
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2023-01-01 12:01:26 -0600

    Started setup for backend user model

---
+++ backend/src/main/java/org/launchcode/backend/service/UserService.java
@@ -0,0 +1,41 @@
+package org.launchcode.backend.service;
+
+import org.launchcode.backend.Repositories.UserRepository;
+import org.launchcode.backend.exceptions.UserNotFoundException;
+import org.launchcode.backend.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user){
+      return  userRepository.save(user);
+    }
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User findUserById(Long id){
+        return (User) userRepository.findUserById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteUser (Long id)    {
+        userRepository.deleteUserById(id);
+    }
+
+
+}
