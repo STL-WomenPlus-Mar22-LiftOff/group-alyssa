566deeaf80a6226361fe57aa8e44a8253bef3347
rebaderbs <rderbak@gmail.com>
2023-01-05 14:38:34 -0600

    create-account page layout completed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { CreateTripComponent } from './create-trip/create-trip.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ViewAllTripsComponent } from './view-all-trips/view-all-trips.component';
+import { ViewIndividualTripComponent } from './view-individual-trip/view-individual-trip.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
   { path: 'landing-page', component: LandingPageComponent},
+  { path: 'create-account', component: CreateAccountComponent},
+  { path: 'login', component: LogInComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'create-trip', component: CreateTripComponent},
+  { path: 'view-all', component: ViewAllTripsComponent},
+  { path: 'view-individual', component: ViewIndividualTripComponent},
 ];
 
 @NgModule({
