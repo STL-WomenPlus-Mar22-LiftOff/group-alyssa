30ea0160245291b143dfe8722551a183bd58a5c6
Gabrielle <gabrielle.renee1004@gmail.com>
2023-01-05 18:57:32 -0600

    Added abstract enitiy class

---
+++ backend/src/main/java/org/launchcode/backend/models/AbstractEntity.java
@@ -0,0 +1,35 @@
+package org.launchcode.backend.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity entity = (AbstractEntity) o;
+        return id.equals(entity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
+
+
+
