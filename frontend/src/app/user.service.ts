1f8604bec73a17e2aa27334a051277ec883fde1a
rebaderbs <rderbak@gmail.com>
2023-01-11 11:38:23 -0600

    working attempt at connecting front/back ends

---
+++ frontend/src/app/user.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl:string;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = "http://localhost:8080/user";
+   }
+
+   //below should ALL mirror what is on the UserResource.java file (on the backend) as what each does, and the url it returns and .get, .post, etc.
+
+   //returns all users at http://localhost:8080/user/all
+   public getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${this.baseUrl}`);
+   }
+
+   //add user; returns only that User object, instead of an array
+   public addUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/add`, user);
+   }
+
+   public updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.baseUrl}/update`, user);
+   }
+
+   //delete user by id; void as it's returning nothing
+   public deleteUser(userId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/delete/${userId}`);
+   }
+
+
+}
---
+++ frontend/src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data: User[]) => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+
+}
