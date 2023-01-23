62af43fe53a4db3394a6fc74adc236e737b0a411
rebaderbs <rderbak@gmail.com>
2023-01-23 14:10:39 -0600

    create trip button routing to default view-trip page; not saving to user_id so will need to configure that

--- backend/src/main/java/org/launchcode/backend/TripResource.java
+++ backend/src/main/java/org/launchcode/backend/TripResource.java
@@ -1,49 +1,60 @@
 package org.launchcode.backend;
 
+import org.launchcode.backend.Repositories.TripRepository;
 import org.launchcode.backend.models.Trip;
+import org.launchcode.backend.models.User;
 import org.launchcode.backend.service.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/trip")
-
 public class TripResource {
-        private final TripService tripService;
 
-        public TripResource(TripService tripService){
-            this.tripService = tripService;
-        }
+    @Autowired
+    private TripRepository tripRepository;
+    private final TripService tripService;
 
-        //method to return all trips in application
-       @GetMapping
-       public ResponseEntity<Iterable<Trip>> getAllTrips(){
-         Iterable<Trip> trip = tripService.findAllTrips();
-            return new ResponseEntity<>(trip, HttpStatus.OK);
-        }
+    public TripResource(TripService tripService){ this.tripService = tripService;}
 
-      // find a trip
-        @GetMapping("/find/{id}")
-        public ResponseEntity<Trip> getTripById(@PathVariable("id") Long id){
-           Trip trip = tripService.findById(id);
-            return new ResponseEntity<>(trip, HttpStatus.OK);
-        }
+    //method to return all trips in application
+    @GetMapping("")
+    public ResponseEntity<Iterable<Trip>> getAllTrips(){
+        Iterable<Trip> trip = tripService.findAllTrips();
+        return new ResponseEntity<>(trip, HttpStatus.OK);
+    }
 
-        //add a trip
-        //Post b/c we are changing things on backend
-        @PutMapping("/update")
-        public ResponseEntity<Trip> updateTrip(@RequestBody Trip trip){
-           Trip updateTrip = tripService.updateTrip(trip);
-            return new ResponseEntity<>(updateTrip, HttpStatus.OK);
-        }
+    //add a trip
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public void addTrip(@RequestBody Trip trip)    {
+        Trip newTrip = new Trip(trip.getTripName(), trip.getStartingLocation(), trip.getEndingLocation());
+        tripRepository.save(newTrip);
+    }
 
-        //delete trip
-        @DeleteMapping("/delete/{id}")
-        public ResponseEntity<?> deleteTrip(@PathVariable("id") Long id){
-           tripService.deleteTrip(id);
-            //only returned Http b/c user was deleted so there isn't anything to show
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
+    // find a trip
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Trip> getTripById(@PathVariable("id") Long id){
+       Trip trip = tripService.findById(id);
+        return new ResponseEntity<>(trip, HttpStatus.OK);
+    }
+
+
+    //Post b/c we are changing things on backend
+    @PutMapping("/update")
+    public ResponseEntity<Trip> updateTrip(@RequestBody Trip trip){
+       Trip updateTrip = tripService.updateTrip(trip);
+        return new ResponseEntity<>(updateTrip, HttpStatus.OK);
+    }
+
+    //delete trip
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteTrip(@PathVariable("id") Long id){
+       tripService.deleteTrip(id);
+        //only returned Http b/c user was deleted so there isn't anything to show
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
 
--- frontend/src/app/trip/trip.component.ts
+++ frontend/src/app/trip/trip.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from '../trip';
+import { TripService } from '../trip.service';
 
 @Component({
   selector: 'app-trip',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripComponent implements OnInit {
 
-  constructor() { }
+  trips: Trip[] = [];
+
+  constructor(private tripService: TripService) { }
 
   ngOnInit(): void {
+
   }
 
 }
